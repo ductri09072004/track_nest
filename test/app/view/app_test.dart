597ab305ebf4c706434d8da34abff3fa41a5bf32
BlueDuck <blueduck09072004@gmail.com>
2025-01-07 21:12:02 +0700

    fix: file test

---
+++ test/app/view/app_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:testverygood/app/app.dart';
+import 'package:testverygood/feature/history.dart';
+import 'package:testverygood/feature/split_money.dart';
+import 'package:testverygood/feature/statistical.dart';
+
+void main() {
+  group('App Widget Tests', () {
+    testWidgets('renders App widget and finds CounterPage', (tester) async {
+      // Khởi chạy widget App
+      await tester.pumpWidget(const App());
+
+      // Kiểm tra xem CounterPage có xuất hiện hay không
+      expect(find.byType(SplitMoneyPage), findsOneWidget);
+      expect(find.byType(HistoryPage), findsOneWidget);
+      expect(find.byType(StatisticalPage), findsOneWidget);
+    });
+
+    // Test bổ sung: Đảm bảo không có lỗi trong widget App
+    testWidgets('App renders without throwing', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(tester.takeException(), isNull);
+    });
+  });
+}
