abfa2675031a3bd4855acd72b33f830d505ba624
BlueDuck <blueduck09072004@gmail.com>
2025-01-07 13:42:42 +0700

    first

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/testverygood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.testverygood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:testverygood/app/app.dart';
+import 'package:testverygood/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
