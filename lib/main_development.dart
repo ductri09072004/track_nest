d3727a3f8ce47d0221e7950134ddccb758cbb3c0
Lê Thị Tuyết Trâm <142717464+LeChayS@users.noreply.github.com>
2025-01-18 12:37:55 +0700

    Revert "chore: setup firebasefirebase"
    This reverts commit c262836731f1859c9823867924da11d7463921aa.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,5 @@
 import 'package:testverygood/app/app.dart';
 import 'package:testverygood/bootstrap.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
 
 void main() {
   bootstrap(() => const App());
